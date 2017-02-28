c9841d1b4328349f08ffbc88187061bf90e15345
Demar Swack <demarswack@Demars-MacBook-Pro.local>
2017-02-28 17:48:18 -0500

    Init commit

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,26 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to Mongo server');
+  }
+  console.log('Connect to MongoDB server');
+
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('58b49c90a6ca5261944ff20a')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+    db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  //db.close();
+});
